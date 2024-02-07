6d7de89bc77c28a314eb0c81124d58e724cbac6a
Victor Matheus <victormatheus507@gmail.com>
2024-02-07 09:08:55 -0300

    css constantes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,38 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+  },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
+    "react-refresh/only-export-components": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    'no-unused-vars': 'off',
     'react-refresh/only-export-components': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-  },s
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unsafe-finally': 'off',
+    'react/jsx-key': 'off',
+  },
 };
