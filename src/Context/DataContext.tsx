937380e4776f5c5c0ac68411379cb4e6b8bc6c16
Victor Matheus <victormatheus507@gmail.com>
2024-02-07 09:41:46 -0300

    Data Reativa

--- src/Context/DataContext.tsx
+++ src/Context/DataContext.tsx
@@ -5,6 +5,10 @@ type IDataContext = {
   data: IVenda[] | null
   loading: boolean,
   error: string | null,
+  inicio: string,
+  final: string,
+  setInicio: React.Dispatch<React.SetStateAction<string>>,
+  setFinal: React.Dispatch<React.SetStateAction<string>>,
 }
 
 type IVenda = {
@@ -21,14 +25,28 @@ const DataContext = React.createContext<IDataContext | null>(null)
 
 export const useData = () => {
   const context = React.useContext(DataContext)
-  if(!context) throw new Error('useData precisa estar em DataContextProvider')
+  if (!context) throw new Error('useData precisa estar em DataContextProvider')
 
   return context
 }
 
-export const DataContextProvider = ({children}: React.PropsWithChildren) => {
+function getDate(n: number) {
+  const date = new Date()
+  date.setDate(date.getDate() - n)
+  const dd = String(date.getDate()).padStart(2, '0')
+  const mm = String(date.getMonth() + 1).padStart(2, '0')
+  const yyyy = date.getFullYear()
 
-  const {data, loading, error} = useFetch<IVenda[]>('https://data.origamid.dev/vendas/')
+  return `${yyyy}-${mm}-${dd}`
+}
 
-  return <DataContext.Provider value={{data, loading, error}} >{children}</DataContext.Provider>
+
+export const DataContextProvider = ({ children }: React.PropsWithChildren) => {
+
+  const [inicio, setInicio] = React.useState(getDate(30))
+  const [final, setFinal] = React.useState(getDate(0))
+
+  const { data, loading, error } = useFetch<IVenda[]>(`https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`)
+
+  return <DataContext.Provider value={{ data, loading, error, inicio, setInicio, final, setFinal }} >{children}</DataContext.Provider>
 }
